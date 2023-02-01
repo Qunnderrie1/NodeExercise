6724c3929d9ae1f0c9e9117382a495c1bbd71269
qunnderrie1 <qunnderrie@gmail.com>
2023-02-01 13:28:31 -0500

    made changes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const http = require("http");
 const port = 5000;
 
 const server = http.createServer((req, res) =>{
-    res.end("Hello World!");
+
+    res.writeHead(200, {"content-type" : "text/html"})
+    res.write("Hello World!")
+    res.end();
+}).listen(port , () => {
+    console.log("Server is listeing on port 5000....")
 })
-
-
-server.listen(port);
